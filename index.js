9533193f40daf74b981dd23c0a15456523f963b4
James Lal <james@lightsofapollo.com>
2013-10-24 11:04:00 -0700

    simplify and only use sync api

--- index.js
+++ index.js
@@ -1,38 +1,19 @@
-var SettingsApi = require('./lib/settingsapi');
-
-
 /**
  * @constructor
- * @param {Apps} apps private marionette apps api. ???
+ * @param {Marionette.Client} client Marionette client to use.
  */
-function MarionetteSettingsApi(settingsApi) {
-  for (var key in MarionetteSettingsApi.ModuleToFunction) {
-    var methodName = MarionetteSettingsApi.ModuleToFunction[key];
-    var module = require('./lib/' + key);
-    // bind the public interface to the object
-    this[methodName] = module[methodName].bind(null, settingsApi);
-  }
+function SettingsApi(client) {
+  this._client = client;
 }
 
-/**
- * Map of public modules to expose to the main interface.
- * Key is module, value is function on the module.
- */
-MarionetteSettingsApi.ModuleToFunction = {
-  getsetting: 'getSetting',
-  setsetting: 'setSetting'
+SettingsApi.prototype = {
+  get: function(name) {
+    return require('./lib/getsetting')(this._client, name);
+  }
 };
 
 
-/**
- * Create a new marionette settings api plugin.
- * @param {Marionette.Client} client marionette client to use.
- * @param {Objects} options optional configuration.
- * @return {MarionetteSettingsApi} new marionette settings.
- */
-function setup(client, options) {
-  var settingsApi = MarionetteSettingsApi.setup(client, options);
-  return new MarionetteSettingsApi(settingsApi);
-}
+module.exports = function(client) {
+  return new SettingsApi(client);
+};
 
-module.exports.setup = setup;
